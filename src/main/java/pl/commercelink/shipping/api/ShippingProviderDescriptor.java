5bd5db389987032e48a843f97453b62ecda3f1ad
Marcin Dziedzic <marcin@madmangaming.pl>
2026-03-10 10:51:11 +0100

    add shipping provider descriptor

---
+++ src/main/java/pl/commercelink/shipping/api/ShippingProviderDescriptor.java
@@ -0,0 +1,6 @@
+package pl.commercelink.shipping.api;
+
+import pl.commercelink.provider.api.ProviderDescriptor;
+
+public interface ShippingProviderDescriptor extends ProviderDescriptor<ShippingProvider> {
+}
